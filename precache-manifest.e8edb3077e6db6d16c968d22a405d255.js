49be47c35903d09000066bcb4884899d2b51bbf5
Ryan Wolfe <rwolfe22@gmail.com>
2019-10-23 21:51:45 -0700

    Updates

---
+++ precache-manifest.e8edb3077e6db6d16c968d22a405d255.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "59ff2d534395404ec64a9dcb94861453",
+    "url": "/responsive-tables/index.html"
+  },
+  {
+    "revision": "972ff96d4dc8c35c3e43",
+    "url": "/responsive-tables/static/css/main.e361f24b.chunk.css"
+  },
+  {
+    "revision": "3969fab1ad6e79c2791d",
+    "url": "/responsive-tables/static/js/2.ca984729.chunk.js"
+  },
+  {
+    "revision": "972ff96d4dc8c35c3e43",
+    "url": "/responsive-tables/static/js/main.d06bcb79.chunk.js"
+  },
+  {
+    "revision": "16c1dd2f2c2b4ac1bbfb",
+    "url": "/responsive-tables/static/js/runtime-main.65d35c17.js"
+  }
+]);
